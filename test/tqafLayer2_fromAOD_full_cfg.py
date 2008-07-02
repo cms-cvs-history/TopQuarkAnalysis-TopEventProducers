2950119df2377d4a2bf2fca8978da469d531991b
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2008-07-02 18:08:55 +0000

    cleanup and comments

--- test/tqafLayer2_fromAOD_full_cfg.py
+++ test/tqafLayer2_fromAOD_full_cfg.py
@@ -1,52 +1,40 @@
 import FWCore.ParameterSet.Config as cms
 
-<<<<<<< tqafLayer2_fromAOD_full_cfg.py
-process = cms.Process("TQAF")
-# initialize MessageLogger
-=======
-##
-# test cfg file for semi-leptonic ttbar analyses
-##
+#-------------------------------------------------
+# test cfg file for tqaflayer1 & 2 production from
+# fullsim for semi-leptonic ttbar events 
+#-------------------------------------------------
 process = cms.Process("TEST")
 
-## add message logger
->>>>>>> 1.2
+## configure message logger
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.threshold = 'INFO'
 
-<<<<<<< tqafLayer2_fromAOD_full_cfg.py
-#
-# tqaf steering
-#
-process.load("TopQuarkAnalysis.TopObjectProducers.tqafLayer1_full_cff")
-
-process.load("TopQuarkAnalysis.TopEventProducers.tqafLayer2_ttSemiLeptonic_cff")
-
-process.load("TopQuarkAnalysis.TopEventProducers.tqafLayer2_EventContent_cff")
-
-process.load("TopQuarkAnalysis.TopObjectProducers.tqafLayer1_EventContent_cff")
-
-process.options = cms.untracked.PSet(
-    wantSummary = cms.untracked.bool(True)
-)
-=======
-##
+#-------------------------------------------------
 # process configuration
-##
->>>>>>> 1.2
+#-------------------------------------------------
+
+## define input
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring('file:/afs/cern.ch/cms/PRS/top/cmssw-data/relval200-for-pat-testing/FullSimTTbar-210p5.1-AODSIM.100.root')
 )
+
+## define maximal number of events to loop over
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(100)
 )
+
+## configure process options
 process.options = cms.untracked.PSet(
     wantSummary = cms.untracked.bool(True)
 )
 
-##
-# tqaf configuration
-##
+#-------------------------------------------------
+# tqaf configuration; if you want just to produce 
+# tqafLayer2 on top of an already existing
+# tqafLayer1 just comment the standard tqafLayer1
+# production sequence
+#-------------------------------------------------
 
 ## std sequence for tqaf layer1
 process.load("TopQuarkAnalysis.TopObjectProducers.tqafLayer1_full_cff")
@@ -54,14 +42,17 @@ process.load("TopQuarkAnalysis.TopObjectProducers.tqafLayer1_full_cff")
 ## std sequence for tqaf layer2 for semi-leptonic decays
 process.load("TopQuarkAnalysis.TopEventProducers.tqafLayer2_ttSemiLeptonic_cff")
 
-##
-# add tqaf event content
-##
+#-------------------------------------------------
+# tqaf event content; first ALL objects are
+# dropped in this process; then tqafLayer1 and
+# tqafLayer2 concent is added
+#-------------------------------------------------
 
 ## define event content
 process.tqafEventContent = cms.PSet(
     outputCommands = cms.untracked.vstring('drop *')
 )
+
 ## define tqaf layer1 event content
 process.load("TopQuarkAnalysis.TopObjectProducers.tqafLayer1_EventContent_cff")
 process.tqafEventContent.outputCommands.extend(process.patLayer1EventContent.outputCommands)
@@ -70,9 +61,13 @@ process.tqafEventContent.outputCommands.extend(process.patLayer1EventContent.out
 process.load("TopQuarkAnalysis.TopEventProducers.tqafLayer2_EventContent_cff")
 process.tqafEventContent.outputCommands.extend(process.tqafLayer2TtSemiLeptonicEventContent.outputCommands)
 
-##
-# define process output
-##
+#-------------------------------------------------
+# process output; first the event selection is
+# defined: only those events that have passed the
+# full production path are selected and written
+# to file; the event content has been defined
+# above
+#-------------------------------------------------
 
 ## define event selection
 process.EventSelection = cms.PSet(
@@ -88,6 +83,12 @@ process.out = cms.OutputModule("PoolOutputModule",
     verbose = cms.untracked.bool(False),
     fileName = cms.untracked.string('TQAFLayer2_Output.fromAOD_full.root')
 )
+
+#-------------------------------------------------
+# output paths; in order not to write the
+# persistent output to file comment the the output
+# path
+#-------------------------------------------------
 
 ## process path
 process.p = cms.Path(process.tqafLayer1*process.tqafLayer2_ttSemiLeptonic)
