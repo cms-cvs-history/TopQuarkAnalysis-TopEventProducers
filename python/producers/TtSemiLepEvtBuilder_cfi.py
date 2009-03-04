a0b15aff40e3508f98bb2ab3ef383f1f5d8e2574
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2009-03-04 20:01:53 +0000

    overhauling of TtSemiLepJetComb class; it can also take user defined variables now

--- python/producers/TtSemiLepEvtBuilder_cfi.py
+++ python/producers/TtSemiLepEvtBuilder_cfi.py
@@ -17,7 +17,7 @@ ttSemiLepEvent = cms.EDProducer("TtSemiLepEvtBuilder",
 
     ## set verbosity level
     verbosity = cms.int32(0),  # 0: no additional printout
-                               # 1: print a summary for each event
+                               # N: print a summary for N events
 
     ## add genEvt (if available)
     genEvent = cms.InputTag("genEvt"),
@@ -29,13 +29,13 @@ ttSemiLepEvent = cms.EDProducer("TtSemiLepEvtBuilder",
                               # can be set to -1 to take all jets
 
     ## labels for event hypotheses
-    hyps = cms.vstring("ttSemiLepHypGeom",
-                       "ttSemiLepHypWMassMaxSumPt",
-                       "ttSemiLepHypMaxSumPtWMass",
-                       "ttSemiLepHypGenMatch",
-                       "ttSemiLepHypKinFit",
-                       "ttSemiLepHypMVADisc"
-                       ),
+    hypotheses = cms.vstring("ttSemiLepHypGeom",
+                             "ttSemiLepHypWMassMaxSumPt",
+                             "ttSemiLepHypMaxSumPtWMass",
+                             "ttSemiLepHypGenMatch",
+                             "ttSemiLepHypKinFit",
+                             "ttSemiLepHypMVADisc"
+                             ),
                                 
     ## add extra information on kinFit
     kinFit = cms.PSet(
