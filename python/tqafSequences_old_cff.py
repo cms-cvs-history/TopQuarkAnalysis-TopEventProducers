a9550bbb9267210b434b7162a18ff97731853bc1
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2009-03-17 15:01:56 +0000

    roundup

--- python/tqafSequences_old_cff.py
+++ python/tqafSequences_old_cff.py
@@ -2,9 +2,15 @@ import FWCore.ParameterSet.Config as cms
 
 ## produce ttGenEvent
 from TopQuarkAnalysis.TopEventProducers.sequences.ttGenEvent_cff import *
-
 ## produce event solution
 from TopQuarkAnalysis.TopEventProducers.producers.TtSemiEvtSolProducer_cfi import *
-
 ## make tqaf layer2
-tqafLayer2_ttSemiLeptonic_oldStyle = cms.Sequence(makeGenEvt * solutions)
+tqafLayer2_ttSemiLeptonic_old = cms.Sequence(makeGenEvt * solutions)
+
+
+## produce ttGenEvent
+from TopQuarkAnalysis.TopEventProducers.sequences.ttGenEvent_cff import *
+## produce event solution
+from TopQuarkAnalysis.TopEventProducers.producers.TtDilepEvtSolProducer_cfi import *
+## make tqaf layer2
+tqafLayer2_ttFullLeptonic_old = cms.Sequence(makeGenEvt*solutions)
