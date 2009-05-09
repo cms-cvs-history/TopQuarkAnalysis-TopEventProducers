6684b2aef3d2f5a398aed51cc78427fe62dcb418
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2009-05-09 21:49:51 +0000

    roundup final step

--- test/ttDecaySelection_cfg.py
+++ test/ttDecaySelection_cfg.py
@@ -4,53 +4,49 @@ process = cms.Process("TEST")
 
 ## add message logger
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
-## print original listing of gen particles
-## process.MessageLogger.categories.append('TopDecaySubset::printSource')
-## print final pruned listing of top decay chain
-## process.MessageLogger.categories.append('TopDecaySubset::printTarget')
-## print final pruned listing of top decay chain
-process.MessageLogger.categories.append('TtDecayChannelSelector::selection')
+## add categories
+## process.MessageLogger.categories.append('TopDecaySubset_printSource')
+process.MessageLogger.categories.append('TopDecaySubset_printTarget')
+process.MessageLogger.categories.append('TtDecayChannelSelector_selection')
+## configure cout
 process.MessageLogger.cout = cms.untracked.PSet(
  INFO = cms.untracked.PSet(
    limit = cms.untracked.int32(0),
-   decayChain = cms.untracked.PSet( limit = cms.untracked.int32(10) )
+## TopDecaySubset_printSource       = cms.untracked.PSet( limit = cms.untracked.int32(10) ),
+   TopDecaySubset_printTarget       = cms.untracked.PSet( limit = cms.untracked.int32(10) ),
+   TtDecayChannelSelector_selection = cms.untracked.PSet( limit = cms.untracked.int32(10) )   
   )
 )
 
 ## define input
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-   'file:/afs/cern.ch/cms/PRS/top/cmssw-data/relval200-for-pat-testing/FullSimTTBar-2_2_X_2008-11-03-STARTUP_V7-AODSIM.100.root'
+    '/store/relval/CMSSW_3_1_0_pre6/RelValTTbar/GEN-SIM-RECO/IDEAL_31X_v1/0002/50D4BADB-FA32-DE11-BA01-000423D98DC4.root'
     )
 )
-
 ## define maximal number of events to loop over
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(100)
+    input = cms.untracked.int32(10)
 )
-
 ## configure process options
 process.options = cms.untracked.PSet(
     wantSummary = cms.untracked.bool(False)
 )
 
-## configure geometry
+## configure geometry & conditions
 process.load("Configuration.StandardSequences.Geometry_cff")
-
-## configure conditions
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = cms.string('STARTUP_V7::All')
-
-## load magnetic field
 process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = cms.string('IDEAL_31X::All')
 
-## create ttGenEvent
+## configure genEvent
 process.load("TopQuarkAnalysis.TopEventProducers.sequences.ttGenEvent_cff")
 process.decaySubset.addRadiatedGluons = True
 process.genEvt.src = "decaySubset:ME"
+## path1
 process.genEvtProc = cms.Path(process.makeGenEvt)
                
-## std sequence to produce the ttSemiEvent
+## configure decay channel selection 
 process.load("TopQuarkAnalysis.TopEventProducers.producers.TtDecaySelection_cfi")
 process.ttDecaySelection.src = "genEvt"
 process.ttDecaySelection.allowedTopDecays.decayBranchA.electron = True
@@ -60,4 +56,5 @@ process.ttDecaySelection.allowedTopDecays.decayBranchB.muon     = False
 process.ttDecaySelection.allowedTauDecays.leptonic              = True
 process.ttDecaySelection.allowedTauDecays.oneProng              = True
 process.ttDecaySelection.allowedTauDecays.threeProng            = True
+## path2
 process.ttDecaySelectionProc = cms.Path(process.ttDecaySelection)
