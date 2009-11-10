55d49995f3284a2049359df1a663b533e576bff3
Eike Schlieckau <sha1-fc2fe05d7bf2eff84264ececb7f974ab67c284cc@cern.ch>
2009-11-10 14:08:06 +0000

    added kinFit-Hypothesis, corrected comment

--- python/producers/TtFullHadEvtBuilder_cfi.py
+++ python/producers/TtFullHadEvtBuilder_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 #
-# module to fill the full-leptonic ttbar event structure
+# module to fill the full-hadronic ttbar event structure
 #
 ttFullHadEvent = cms.EDProducer("TtFullHadEvtBuilder",
     ## choose leptonic decay modes
@@ -14,7 +14,6 @@ ttFullHadEvent = cms.EDProducer("TtFullHadEvtBuilder",
                                   # 2: muon
                                   # 3: tau
 
-
     ## set verbosity level
     verbosity = cms.int32(0),  # 0: no additional printout
                                # 1: print a summary for each event
@@ -23,9 +22,18 @@ ttFullHadEvent = cms.EDProducer("TtFullHadEvtBuilder",
     genEvent = cms.InputTag("genEvt"),
 
     ## labels for event hypotheses
+    ## (this vector of strings can be modified using the functions
+    ## addTtFullHadHypotheses and removeTtFullHadHypGenMatch in
+    ## TopQuarkAnalysis.TopEventProducers.sequences.ttFullHadEvtBuilder_cff)
     hypotheses = cms.vstring("ttFullHadHypGenMatch"),  # "ttFullHadHypKinFit"
                                                        # "ttFullHadHypMVADisc"
 
+    ## add extra information on kinFit
+    kinFit = cms.PSet(
+        chi2 = cms.InputTag("kinFitTtFullHadEventHypothesis","Chi2"),
+        prob = cms.InputTag("kinFitTtFullHadEventHypothesis","Prob"),
+    ),
+                                
     ## add extra information on genMatch
     genMatch = cms.PSet(
         sumPt = cms.InputTag("ttFullHadJetPartonMatch","SumPt"),
