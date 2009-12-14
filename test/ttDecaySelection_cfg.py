b62f5ccd7005d961305db906d34555f6f53ab906
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2009-12-14 20:58:30 +0000

    adaptations to changes in TopDecayChannelSelector.cc (r1.16.4.1)

--- test/ttDecaySelection_cfg.py
+++ test/ttDecaySelection_cfg.py
@@ -5,6 +5,7 @@ process = cms.Process("TEST")
 ## add message logger
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.categories.append('TopDecaySubset_printTarget')
+#process.MessageLogger.categories.append('TopDecaySubset_printSource')
 process.MessageLogger.categories.append('TtDecayChannelSelector_selection')
 process.MessageLogger.cerr.TopDecaySubset_printTarget = cms.untracked.PSet(
     limit = cms.untracked.int32(10)
@@ -16,12 +17,13 @@ process.MessageLogger.cerr.TtDecayChannelSelector_selection = cms.untracked.PSet
 ## define input
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-    '/store/relval/CMSSW_3_3_0/RelValTTbar/GEN-SIM-RECO/MC_31X_V9-v1/0009/F651D737-75B7-DE11-BDD4-001D09F2512C.root'
+    '/store/user/rwolf/ttbar09/patTuple_all_19_ttbar09.root'
+   #'/store/relval/CMSSW_3_3_0/RelValTTbar/GEN-SIM-RECO/MC_31X_V9-v1/0009/F651D737-75B7-DE11-BDD4-001D09F2512C.root'
     )
 )
 ## define maximal number of events to loop over
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10)
+    input = cms.untracked.int32(100)
 )
 ## configure process options
 process.options = cms.untracked.PSet(
@@ -29,27 +31,29 @@ process.options = cms.untracked.PSet(
 )
 
 ## configure geometry & conditions
-process.load("Configuration.StandardSequences.Geometry_cff")
-process.load("Configuration.StandardSequences.MagneticField_cff")
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = cms.string('MC_31X_V9::All')
+#process.load("Configuration.StandardSequences.Geometry_cff")
+#process.load("Configuration.StandardSequences.MagneticField_cff")
+#process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+#process.GlobalTag.globaltag = cms.string('MC_31X_V9::All')
 
 ## configure genEvent
 process.load("TopQuarkAnalysis.TopEventProducers.sequences.ttGenEvent_cff")
 process.decaySubset.fillMode = "kME"
-process.decaySubset.addRadiatedGluons = True
+process.decaySubset.addRadiation = True
 ## path1
 process.genEvtProc = cms.Path(process.makeGenEvt)
                
 ## configure decay channel selection 
 process.load("TopQuarkAnalysis.TopEventProducers.producers.TtDecaySelection_cfi")
 process.ttDecaySelection.src = "genEvt"
-process.ttDecaySelection.allowedTopDecays.decayBranchA.electron = True
-process.ttDecaySelection.allowedTopDecays.decayBranchA.muon     = True
+process.ttDecaySelection.allowedTopDecays.decayBranchA.electron = False
+process.ttDecaySelection.allowedTopDecays.decayBranchA.muon     = False
+process.ttDecaySelection.allowedTopDecays.decayBranchA.tau      = True
 process.ttDecaySelection.allowedTopDecays.decayBranchB.electron = False
 process.ttDecaySelection.allowedTopDecays.decayBranchB.muon     = False
-process.ttDecaySelection.allowedTauDecays.leptonic              = True
-process.ttDecaySelection.allowedTauDecays.oneProng              = True
-process.ttDecaySelection.allowedTauDecays.threeProng            = True
+process.ttDecaySelection.allowedTopDecays.decayBranchB.tau      = False
+#process.ttDecaySelection.restrictTauDecays.leptonic            = False
+#process.ttDecaySelection.restrictTauDecays.oneProng            = True
+#process.ttDecaySelection.restrictTauDecays.threeProng          = True
 ## path2
 process.ttDecaySelectionProc = cms.Path(process.ttDecaySelection)
