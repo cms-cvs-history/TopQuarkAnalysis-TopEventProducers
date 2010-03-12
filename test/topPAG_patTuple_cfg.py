4e1afd5656971cf221312c216868a3741b56db95
Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch>
2010-03-12 14:46:47 +0000

    adaptations to CMSSW_3_5_X and added l1extras

--- test/topPAG_patTuple_cfg.py
+++ test/topPAG_patTuple_cfg.py
@@ -1,57 +1,60 @@
 ##
 #
 # This is an example PAT configuration as a start off for
-# a recommended Top PAG PAT tuple
+# a recommended Top PAG PAT tuple; NOTE the it contains all
+# information that might be of interest for you. You might
+# want to do changes or adaptations to make this sample
+# suit to your concrete analysis case.
 #
 ##
 
-## Starting with a skeleton process which gets imported with the following line
+## Starting with a skeleton process, which gets imported with the following line
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
 
 ##
 # Reconfigure the workflow depending on whether you run on data or on MC
 ##
 
-## remove MC matching
+## Remove MC matching
 from PhysicsTools.PatAlgos.tools.coreTools import *
 removeMCMatching(process, ['All'])
 
 ##
 # Top PAG specific modifications of the PAT default (this aims for completeness,
-# not doe disc space optimization)
+# not for disc space optimization)
 ##
 
-## exploit MET
+## Exploit MET
 from PhysicsTools.PatAlgos.tools.metTools import *
 addTcMET(process, 'TC')
 addPfMET(process, 'PF')
 
-## add further jet collections
+## Add further jet collections
 from PhysicsTools.PatAlgos.tools.jetTools import *
 addJetCollection(process,cms.InputTag('ak5PFJets'),
-                 'PF',
+                 'AK5', 'PF',
                  doJTA        = True,
                  doBTagging   = True,
                  jetCorrLabel = ('AK5', 'PF'),
-                 doType1MET   = False,
+                 doType1MET   = False, ## there is no use to apply residual jet corrections on CaloMET 
                  doL1Cleaning = True,                 
                  doL1Counters = False,
                  genJetCollection=cms.InputTag("ak5GenJets"),
-                 doJetID      = False
+                 doJetID      = False  ## there is no use of jetID for Pflow jets
                  )
 
-## produce jpt corrected calo jets, which are not on AOD per default
+## Produce jpt corrected calo jets, which are not on AOD per default
 process.load("PhysicsTools.PatAlgos.recoLayer0.jetPlusTrack_cff")
 process.jpt = cms.Path(
     process.jptCaloJets
 )
 
 addJetCollection(process,cms.InputTag('JetPlusTrackZSPCorJetAntiKt5'),
-                 'JPT',
+                 'AK5', 'JPT',
                  doJTA        = True,
                  doBTagging   = True,
-                 jetCorrLabel = None,
-                 doType1MET   = False,
+                 jetCorrLabel = None,  ## jpt is viewed as a jet correction; there is no residual 
+                 doType1MET   = False, ## correction on top of that; there is also no MET correction
                  doL1Cleaning = True,
                  doL1Counters = True,                 
                  genJetCollection = cms.InputTag("ak5GenJets"),
@@ -59,35 +62,39 @@ addJetCollection(process,cms.InputTag('JetPlusTrackZSPCorJetAntiKt5'),
                  )
 
 
-## set the right sample for JEC factors
-process.jetCorrFactors.corrSample   = 'Summer09_7TeV'
-process.jetCorrFactorsPF.corrSample = 'Summer09_7TeV'
+## Set the right sample for JEC factors here
+process.patJetCorrFactors.corrSample      = 'Summer09_7TeV'
+process.patJetCorrFactorsAK5PF.corrSample = 'Summer09_7TeV'
 
-## apply hadron level correction from ttbar
-## instead of dijet qcd events
-process.jetCorrFactors.sampleType   = 'ttbar'
-process.jetCorrFactorsPF.sampleType = 'ttbar'
+## Apply hadron level corrections as derived
+## from ttbar instead of dijet qcd events
+process.patJetCorrFactors.sampleType      = 'ttbar'
+process.patJetCorrFactorsAK5PF.sampleType = 'ttbar'
 
-## embed calo towers and pflow candidates
-process.allLayer1Jets.embedCaloTowers    = True
-process.allLayer1JetsPF.embedPFCandidates= True
+## Embed calo towers and pflow candidates; you
+## might want to keep the caloTowers instead
+process.patJets.embedCaloTowers           = True
+process.patJetsAK5PF.embedPFCandidates    = True
 
-## run the pat default dequence
+## Run the pat default dequence
 process.p = cms.Path(
-    process.patDefaultSequence
-    )
+  process.patDefaultSequence
+)
 
 ##
-# configure the event content and output
+# Configure the event content and output
 ##
 
-## define output name
+## Define output name
 process.out.fileName = "topPAG_ntuple.root"
 
-## define event content
+## Define event content; keep the cleanPatCandidates which are
+## the same as selectedPatCandidates PLUS some extra information
+## on overlaps across collections.
 from PhysicsTools.PatAlgos.patEventContent_cff import *
 process.out.outputCommands += patExtraAodEventContent
 process.out.outputCommands += ["drop *_towerMaker_*_*"]
+process.out.outputCommands += ["drop l1extraParticles_*_*_*"]
 
 ##
 # configure input
