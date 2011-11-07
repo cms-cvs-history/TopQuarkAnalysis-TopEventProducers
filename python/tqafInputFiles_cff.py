98e1a5a90475734013031e21507aebefdf8f96ba
Sebastian Mirko Naumann-Emme <sha1-4ae0a65b9c10a004316684aa829ec36c9b2fd168@cern.ch>
2011-11-07 13:10:49 +0000

    yet another update due to removed RelVal files

--- python/tqafInputFiles_cff.py
+++ python/tqafInputFiles_cff.py
@@ -1,7 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 from PhysicsTools.PatAlgos.tools.cmsswVersionTools import pickRelValInputFiles
-relValTTbar = pickRelValInputFiles(cmsswVersion  = 'CMSSW_4_2_4',
+relValTTbar = pickRelValInputFiles(cmsswVersion  = 'CMSSW_4_2_9_HLT1',
                                    relVal        = 'RelValTTbar',
-                                   globalTag     = 'MC_42_V12'
+                                   globalTag     = 'MC_42_V14A',
+                                   numberOfFiles = 1
                                    )
