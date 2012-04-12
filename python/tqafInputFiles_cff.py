26d6034ef0dc5bafc114b4c424290a7f6b10cd82
cmsbuild <>
2012-04-12 10:14:31 +0000

    This commit was manufactured by cvs2git to create tag 'TopQuarkAnalysis- TopEventProducers-V06-09-00-01'.
    Sprout from master 2011-11-28 15:52:29 UTC Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch> 'Restore compatibility with RelVal input, starting from CMSSW_5_0_0_pre6.'
    Cherrypick from TopQuarkAnalysis-TopEventProducers-B5_1_X_noDAS 2012-04-12 10:14:30 UTC Volker Adler <sha1-9f2760f111a0fba7f6e1c2c323083a962f99e79c@cern.ch> 'Switch off DAS usage.':
        python/tqafInputFiles_cff.py

--- python/tqafInputFiles_cff.py
+++ python/tqafInputFiles_cff.py
@@ -1,10 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
-from PhysicsTools.PatAlgos.tools.cmsswVersionTools import pickRelValInputFiles
-relValTTbar = pickRelValInputFiles(cmsswVersion  = 'CMSSW_5_0_0_pre6',
-                                   relVal        = 'RelValProdTTbar',
-                                   globalTag     = 'START50_V5',
-                                   dataTier      = 'AODSIM',
-                                   maxVersions   = 1
-                                   )
-
+# from PhysicsTools.PatAlgos.tools.cmsswVersionTools import pickRelValInputFiles
+# relValTTbar = pickRelValInputFiles(cmsswVersion  = 'CMSSW_5_0_0_pre6',
+#                                    relVal        = 'RelValProdTTbar',
+#                                    globalTag     = 'START50_V5',
+#                                    dataTier      = 'AODSIM',
+#                                    maxVersions   = 1
+#                                    )
+relValTTbar = cms.untracked.vstring(
+  '/store/relval/CMSSW_5_0_0/RelValProdTTbar/AODSIM/START50_V8-v3/0082/38FB16A8-542B-E111-A9E6-003048FFCC1E.root',
+  '/store/relval/CMSSW_5_0_0/RelValProdTTbar/AODSIM/START50_V8-v3/0074/E871A010-DD2A-E111-83F5-001A928116CE.root'
+  )
